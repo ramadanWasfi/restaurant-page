05a5c2ecd7e67c98d655af2f51286ecc81ded36c
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-10-02 14:04:42 +0200

    Create Home Page

--- src/home.js
+++ src/home.js
@@ -1,47 +1,80 @@
-import './style.css';
-
 export const createHomePage = () => {
     const container = document.querySelector('#content');
 
-    const header = CreateHeader();
+    const main = createMainContent();
 
-    container.appendChild(header);
+    container.appendChild(main);
 }
 
-const CreateHeader = () => {
-    const header = document.createElement('header');
-    
-    const h1 = document.createElement('h1');
-    h1.textContent = 'La Pizza';
+const createMainContent = () => {
+    const mainContent = document.createElement('div');
+    mainContent.setAttribute('id','mainContent');
 
-    const nav = document.createElement('nav');
+    const mainDiv = createMainDiv();
+    const contactUsDiv = createContactUsDiv();
 
-    const ul = document.createElement('ul');
-    ul.setAttribute('id','navigation')
+    mainContent.appendChild(mainDiv);
+    mainContent.appendChild(contactUsDiv);
 
-    const li1 = document.createElement('li');
-    li1.textContent = 'Home';
-    li1.classList.add('link');
-    li1.setAttribute('id','home');
+    return mainContent;
+}
 
-    const li2 = document.createElement('li');
-    li2.textContent = 'Menu';
-    li2.classList.add('link');
-    li2.setAttribute('id','menu');
+const createMainDiv = () => {
+    const container = document.createElement('div');
+    container.setAttribute('id','main-div');
 
-    const li3 = document.createElement('li');
-    li3.textContent = 'Contact Us';
-    li3.classList.add('link');
-    li3.setAttribute('id','contact-us');
+    const p = document.createElement('p');
+    p.textContent = 'Come on down for some delicious pizzas';
+    p.setAttribute('id','main-paragraph');
 
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    ul.appendChild(li3);
+    const p2 = document.createElement('p');
+    p2.textContent = 'Best pizzas in your country';
+    p2.setAttribute('id','secondary-paragraph');
 
-    nav.appendChild(ul);
+    const orderBtn = document.createElement('button');
+    orderBtn.setAttribute('type','button');
+    orderBtn.textContent = 'Order Now';
+    orderBtn.setAttribute('id','orderBtn');
 
-    header.appendChild(h1);
-    header.appendChild(nav);
+    container.appendChild(p);
+    container.appendChild(p2);
+    container.appendChild(orderBtn);
 
-    return header;
+    return container;
+}
+
+const createContactUsDiv = () => {
+    const container = document.createElement('div');
+    container.setAttribute('id','contact-us-div');
+
+    const addressDiv = document.createElement('div');
+    addressDiv.classList.add('contact-us');
+
+    const addressHeader = document.createElement('p');
+    addressHeader.textContent = "Address";
+
+    const address = document.createElement('p');
+    address.textContent = "1024 Oakwood Ave San Diego, CA 22434";
+    address.setAttribute('id','address');
+
+    addressDiv.appendChild(addressHeader);
+    addressDiv.appendChild(address);
+
+    const HoursDiv = document.createElement('div');
+    HoursDiv.classList.add('contact-us');
+
+    const hoursHeader = document.createElement('p');
+    hoursHeader.textContent = "Hours";
+
+    const hours = document.createElement('p');
+    hours.textContent = "Mon-Thurs:   8am-8pm";
+    hours.setAttribute('id','hours');
+
+    HoursDiv.appendChild(hoursHeader);
+    HoursDiv.appendChild(hours);
+
+    container.appendChild(addressDiv);
+    container.appendChild(HoursDiv);
+
+    return container;
 }
