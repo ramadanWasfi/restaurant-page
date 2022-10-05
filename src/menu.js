3d743647b78b9171d769b1ce86112dd76af257d2
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-10-05 14:33:30 +0200

    Add createMenuHeader function

---
+++ src/menu.js
@@ -0,0 +1,12 @@
+export const createMenuPage = () => {
+    
+}
+
+
+const createMenuHeader = () => {
+    const menuHeader = document.createElement('p');
+    menuHeader.textContent = 'Menu';
+    menuHeader.setAttribute('id','menuHeader');
+
+    return menuHeader;
+}
